a1287ef9ea53f9c9e6fb2e2b7afc02d9e8b80cdf
Shane <shanej97809@gmail.com>
2024-04-11 19:42:23 -0700

    added new functions creating DataFrames

--- main.py
+++ main.py
@@ -5,11 +5,26 @@ import datetime
 
 def data_parse(csv):
     data = pd.read_csv(csv)
-    print(data)
+    return data
+
+
+def data_constructor(df,year=None,state=None):
+    '''
+    This function takes in a given dataframe object,
+    and creates a new DataFrame based on specific types
+    of inputs give
+    '''
+    filtered_data = df[(df['Indicator'] == 'Chlamydia') & (df['Year'] == year) & (df['Geography']==state)]
+    return filtered_data
+
+
 
 def main():
     input_file = "chlamydia_by_state.csv"
-    data_parse(input_file)
+    df = data_parse(input_file)
+    for i in range(2000,2016):
+        print(data_constructor(df,i,"California"))
 
 if __name__ == "__main__":
     main()
+
