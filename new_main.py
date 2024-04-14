5ace8a298fdcf5799e01bd4912f5508cbd2edb04
Shane <shanej97809@gmail.com>
2024-04-13 17:27:39 -0700

    few changes

--- new_main.py
+++ new_main.py
@@ -2,28 +2,14 @@ import pandas as pd
 import datetime
 import matplotlib.pyplot
 import datetime
+import graphConstructor as graph
 
-def data_parse(csv):
-    data = pd.read_csv(csv)
-    return data
-
-
-def data_constructor(df,year=None,state=None):
-    '''
-    This function takes in a given dataframe object,
-    and creates a new DataFrame based on specific types
-    of inputs give
-    '''
-    filtered_data = df[(df['Indicator'] == 'Chlamydia') & (df['Year'] == year) & (df['Geography']==state)]
-    return filtered_data
-
-
-
+#Main area for creating specific graphs, correlations,etc
 def main():
-    input_file = "chlamydia_by_state.csv"
-    df = data_parse(input_file)
+    input_file = input("Please enter the given csv file: ")
+    df = graph.constructor(input_file)
     for i in range(2000,2016):
-        print(data_constructor(df,i,"California"))
+        print(graph.data_constructor(df,i,"California"))
 
 if __name__ == "__main__":
     main()
